0517a492e301889e4ea9da97539d2a63030ef643
Akul Kapoor <akulkapoor@gmail.com>
2012-12-01 17:10:30 -0500

    BackBeat

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	development: {
+		fb: {
+			appID: "467664373275243",
+			appSecret: "d9188d59b654d87746ae463c15d7cf66",
+			url: "http://128.237.134.199:3000/"
+		},
+		dbUrl: "mongodb://128.237.134.199/abc"	
+	}
+}
