e6250c554d6509f36e27e03954df69dad71c96f5
Akul Kapoor <akulkapoor@gmail.com>
2012-12-01 20:38:26 -0500

    asdasd

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ module.exports = {
 		fb: {
 			appID: "467664373275243",
 			appSecret: "d9188d59b654d87746ae463c15d7cf66",
-			url: "http://128.237.134.199:3000/"
+			url: "http://128.237.150.95:3000/"
 		},
-		dbUrl: "mongodb://128.237.134.199/abc"	
+		dbUrl: "mongodb://128.237.150.95/abc"	
 	}
 }
